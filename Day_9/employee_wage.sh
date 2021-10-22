faab38e645fd28e9630d05e6483567b64a99892d
Gowtham G <gowthamgeg3@gmail.com>
2021-10-22 18:44:04 +0530

    Assignment

--- Day_9/employee_wage.sh
+++ Day_9/employee_wage.sh
@@ -9,15 +9,24 @@ case $num in
 	1)
 		echo "Employee is present as full time "
 		salary=$(( hoursInDay * costPerHour ))
+		monthly=$(( 20 * salary ))
+		
 		echo " Earining per day : $salary "
+		echo " Earining per month : $monthly "
+
 		;;
 	2)
 	        echo " Employee is present as part time "
                 salary=$(( partTimehoursInDay * costPerHour ))
-                echo " Earining per day : $salary "
+                monthly=$(( 20 * salary ))
+        
+		echo " Earining per day : $salary "
+		echo " Earning per month : $monthly "
        	        ;;
 	0)
 	        echo " Employee is absent "
                 echo " Earining per day : $salary "
 	        ;;
+	*)
+		echo " Wrong input please try again later "
 esac
